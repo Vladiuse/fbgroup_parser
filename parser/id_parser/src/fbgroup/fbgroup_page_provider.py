1c604333db38b833fd58c71de726eab1543f3478
vladiuse <vladiuse@gmail.com>
2024-09-26 21:06:35 +0300

    add headers as param

--- parser/id_parser/src/fbgroup/fbgroup_page_provider.py
+++ parser/id_parser/src/fbgroup/fbgroup_page_provider.py
@@ -2,6 +2,27 @@ from .dto import FbGroup
 from .fbgroup_page_converter import PageConverter
 from ..request_sender import FbRequestRequestSender
 
+HEADERS = {
+    'authority': 'www.facebook.com',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    'accept-language': 'uk-UA,uk;q=0.9,en-US;q=0.8,en;q=0.7',
+    'dpr': '2',
+    'sec-ch-prefers-color-scheme': 'dark',
+    'sec-ch-ua': '"Not A(Brand";v="99", "Google Chrome";v="121", "Chromium";v="121"',
+    'sec-ch-ua-full-version-list': '"Not A(Brand";v="99.0.0.0", "Google Chrome";v="121.0.6167.75", "Chromium";v="121.0.6167.75"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-model': '""',
+    'sec-ch-ua-platform': '"macOS"',
+    'sec-ch-ua-platform-version': '"13.3.0"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:122.0) Gecko/20100101 Firefox/122.0',
+    'viewport-width': '1325',
+}
+
 class FbPageProvider:
 
     def __init__(self, converter: PageConverter, request_sender: FbRequestRequestSender):
@@ -9,5 +30,5 @@ class FbPageProvider:
         self.request_sender = request_sender
 
     def provide(self, url: str) -> FbGroup:
-        html = self.request_sender.get(url)
+        html = self.request_sender.get(url, headers=HEADERS)
         return self.converter.convert(html=html)
