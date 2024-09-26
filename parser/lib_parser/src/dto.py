0c4995faee9fb10a7dae6706c6a4717f4199465e
vladiuse <vladiuse@gmail.com>
2024-09-26 23:04:37 +0300

    add dto

--- parser/lib_parser/src/dto.py
+++ parser/lib_parser/src/dto.py
@@ -1,4 +1,7 @@
 from dataclasses import dataclass
+from pydantic import BaseModel, Field, model_validator
+from datetime import date
+from .types import AdsStatus
 
 
 @dataclass
@@ -7,3 +10,16 @@ class FbLibAuthData:
     cookies: dict
     data: dict
     session_id: str
+
+
+class FbLibGroupAdsCountRequest(BaseModel):
+    group_id: str = Field(max_length=20, min_length=10)
+    start_date: date
+    end_date: date
+    active_status: AdsStatus
+
+    @model_validator(mode='after')
+    def check_start_less_than_end(self):
+        if self.start_date > self.end_date:
+            raise ValueError('start_date must be more or equal to end_date')
+        return self
