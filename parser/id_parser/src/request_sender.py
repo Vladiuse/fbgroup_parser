b5971ca33ea2d62f53d998fb5b20a2cf8cc8db49
vladiuse <vladiuse@gmail.com>
2024-09-26 22:16:38 +0300

    add post method to request provider

--- parser/id_parser/src/request_sender.py
+++ parser/id_parser/src/request_sender.py
@@ -20,3 +20,26 @@ class FbRequestRequestSender:
         res = req.get(url, **kwargs)
         res.raise_for_status()
         return res.text
+
+    def post(self,
+            url: str,
+            headers: dict | None = None,
+            cookies: dict | None = None,
+            data: dict | None = None,
+            ) -> str:
+        kwargs = {}
+        if headers:
+            kwargs.update({
+                'headers': headers,
+            })
+        if cookies:
+            kwargs.update({
+                'cookies': cookies,
+            })
+        if data:
+            kwargs.update({
+                'data': data,
+            })
+        res = req.post(url, **kwargs)
+        res.raise_for_status()
+        return res.text
