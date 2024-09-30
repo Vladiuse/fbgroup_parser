a22d8424c393d05812942dfe80604c538274f677
vladiuse <vladiuse@gmail.com>
2024-09-30 19:16:53 +0300

    add fb url validator

--- parser/id_parser/src/utils.py
+++ parser/id_parser/src/utils.py
@@ -1,5 +1,27 @@
+import re
 from common.config import config
 import logging
+
+def get_fbgroup_id_from_url(url:str) -> str:
+    url = url.strip()
+    url = url.replace(' ', '')
+    url = url.replace('\n', '')
+    url = url.replace('http://', 'https://')
+    url = url.replace('://www.','://' )
+    if not url.endswith('/'):
+        url = url + '/'
+    patterns = [
+        r'^https://facebook.com/\d{3,30}/$',
+        r'^https://facebook.com/.{3,80}/$',
+        r'^https://fb.com/page-\d{3,30}/$',
+    ]
+    for pattern in patterns:
+        if re.match(pattern, url):
+            url = url[:-1]
+            url = url.replace('https://facebook.com/', '')
+            url = url.replace('https://fb.com/page-', '')
+            return url
+    return ''
 
 
 def get_new_urls() -> set[str]:
@@ -7,7 +29,9 @@ def get_new_urls() -> set[str]:
     with open(config.NEW_URLS_FILE_PATH) as file:
         for line in file:
             url = line.strip()
-            new_urls.add(url)
+            if get_fbgroup_id_from_url(url=url) != '':
+                new_urls.add(url)
     logging.info('Urls in file: %s', len(new_urls))
     return new_urls
+
 
