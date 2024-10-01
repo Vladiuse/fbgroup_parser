12c83d4ae472057c4a616e65ea5e602407b9d1ab
vladiuse <vladiuse@gmail.com>
2024-10-01 22:16:46 +0300

    фвв туц скуфеу ашду ыскшзе

--- parser/common/result_file_creator.py
+++ parser/common/result_file_creator.py
@@ -2,7 +2,7 @@ import csv
 from fb.models import FbGroup as FbGroupModel
 from common.config import config
 
-class ResultFileCreator:
+class AdsCountResultFileCreator:
 
     def create(self, groups: list[FbGroupModel]) -> None:
         with open(config.OUTPUT_FILE_PATH, 'x') as file:
@@ -14,5 +14,17 @@ class ResultFileCreator:
                     'ads_count': group.ads_count,
                 })
 
+class FollowersLikesCountResultFile:
+    def create(self, groups: list[FbGroupModel]) -> None:
+        with open(config.OUTPUT_FILE_PATH, 'x') as file:
+            fieldnames = ['group_url', 'followers_count', 'likes_count']
+            writer = csv.DictWriter(file, delimiter=',', quotechar='"', fieldnames=fieldnames)
+            writer.writeheader()
+            for group in groups:
+                writer.writerow({
+                    'group_url': group.url,
+                    'followers_count': group.followers_count,
+                    'likes_count': group.likes_count,
+                })
 
 
--- parser/scripts/create_result_file.py
+++ parser/scripts/create_result_file.py
@@ -1,5 +1,5 @@
 import logging
-from common.result_file_creator import ResultFileCreator
+from common.result_file_creator import FollowersLikesCountResultFile
 from fb.models import FbGroup as FbGroupModel
 from common.utils import confirm_action
 
@@ -7,9 +7,11 @@ from common.utils import confirm_action
 logging.basicConfig(level=logging.INFO)
 
 
-qs_groups = FbGroupModel.objects.exclude(ads_count__isnull=True).filter(is_used=False).order_by('-ads_count')
-logging.info('groups to write: %s', len(qs_groups))
+# qs_ads_count_groups = FbGroupModel.objects.exclude(ads_count__isnull=True).filter(is_used=False).order_by('-ads_count')
+qs_followers_count = FbGroupModel.objects.filter(followers_count__isnull=False).filter(is_used=False).order_by('-followers_count')
+qs = qs_followers_count
+logging.info('groups to write: %s', len(qs))
 confirm_action()
 
-ResultFileCreator().create(groups=list(qs_groups))
-qs_groups.update(is_used=True)
+FollowersLikesCountResultFile().create(groups=list(qs))
+qs.update(is_used=True)
