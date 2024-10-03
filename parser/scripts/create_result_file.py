e4b8bc822f7e7d4a43a5ed9102cf6461c0bafebd
vladiuse <vladiuse@gmail.com>
2024-10-03 23:05:11 +0300

    fix again

--- parser/scripts/create_result_file.py
+++ parser/scripts/create_result_file.py
@@ -3,16 +3,17 @@ from common.result_file_creator import FollowersLikesCountResultFile
 from fb.models import FbGroup as FbGroupModel
 from common.utils import confirm_action
 
-
 logging.basicConfig(level=logging.INFO)
 
-
 # qs_ads_count_groups = FbGroupModel.objects.exclude(ads_count__isnull=True).filter(is_used=False).order_by('-ads_count')
-qs_followers_count = FbGroupModel.objects.filter(followers_count__isnull=False).filter(is_used=False).order_by('-followers_count')
+qs_followers_count = FbGroupModel.objects.filter(followers_count__isnull=False).filter(is_used=False).order_by(
+    '-followers_count')
 qs = qs_followers_count
+
 logging.info('groups to write: %s', len(qs))
 confirm_action()
 groups_len = int(input('Set len of groups: '))
 qs = qs[:groups_len]
+
 FollowersLikesCountResultFile().create(groups=list(qs))
-print(qs.update(is_used=True))
+print('Records update:', FbGroupModel.objects.filter(pk__in=[g.pk for g in qs]).update(is_used=True))
